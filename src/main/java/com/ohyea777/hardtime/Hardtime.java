19262451b33f6c6dbaee3ee7b2919088e4cc4574
OhYea777 <jacktaylor12345@gmail.com>
2014-08-07 00:50:50 +1200

    Initial commit

---
+++ src/main/java/com/ohyea777/hardtime/Hardtime.java
@@ -0,0 +1,12 @@
+package com.ohyea777.hardtime;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Hardtime extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+
+    }
+
+}
